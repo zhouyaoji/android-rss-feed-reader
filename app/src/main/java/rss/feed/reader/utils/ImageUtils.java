c706121bc441d7a574cd89c3e7ac5f9039d4fa3f
Andriy Ksenych <aksenych@softjourn.com>
2016-09-01 13:05:47 +0300

    rfr-10 Integrate lib for image loading.

---
+++ app/src/main/java/rss/feed/reader/utils/ImageUtils.java
@@ -0,0 +1,64 @@
+package rss.feed.reader.utils;
+
+import android.support.annotation.NonNull;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import rss.feed.reader.RssFeedReaderApp;
+
+/**
+ * Created by Andriy Ksenych on 01.09.2016.
+ */
+public class ImageUtils {
+
+    public interface ImageCallback {
+        void onSuccess(ImageView imageView);
+
+        void onError(String path, ImageView imageView);
+    }
+
+    private static final Picasso PICASSO = Picasso.with(RssFeedReaderApp.getInstance());
+
+    /**
+     * Load image from internet to imageView without loading callback
+     *
+     * @param path      - image url
+     * @param imageView - view of image
+     */
+    public static void load(@NonNull String path, @NonNull ImageView imageView) {
+        PICASSO.load(path).into(imageView);
+    }
+
+    /**
+     * Load image from internet to imageView with loading callback
+     *
+     * @param path      - image url
+     * @param imageView - view of image
+     * @param callback  - {@link ImageCallback} callback with onSuccess and onError methods loading image url
+     */
+    public static void load(@NonNull final String path, @NonNull final ImageView imageView, @NonNull final ImageCallback callback) {
+        PICASSO.load(path).into(imageView, new Callback() {
+            @Override
+            public void onSuccess() {
+                callback.onSuccess(imageView);
+            }
+
+            @Override
+            public void onError() {
+                callback.onError(path, imageView);
+            }
+        });
+    }
+
+    /**
+     * Load image from app resources to imageView
+     * @param resId - id of image resource
+     * @param imageView - view of image
+     */
+    public static void load(int resId, @NonNull ImageView imageView) {
+        PICASSO.load(resId).into(imageView);
+    }
+
+}

0dc3f17af6bfda775affa5e075be31e62bfbd1e5
Andriy Ksenych <aksenych@softjourn.com>
2016-10-26 11:53:52 +0300

    rfr-8 Add Settings > Channel page fixed Changed access modifiers of some fields and method. Added @MainThread annotations

--- app/src/main/java/rss/feed/reader/managers/ChannelManager.java
+++ app/src/main/java/rss/feed/reader/managers/ChannelManager.java
@@ -1,35 +1,62 @@
 package rss.feed.reader.managers;
 
 import android.content.SharedPreferences;
+import android.support.annotation.MainThread;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.util.List;
 
-import javax.inject.Inject;
-
-import rss.feed.reader.RssFeedReaderApp;
 import rss.feed.reader.api.model.Channel;
 
 
 /**
  * Created by Andriy Ksenych on 17.10.2016.
  * Manager for saving/managing user input channels list
+ * It isn't multithreading save
  */
 
 public class ChannelManager {
 
     private static final String PREF_CHANNELS_LIST = "channels list";
 
-    public Gson mGson;
-    public SharedPreferences mPreferences;
+    private Gson mGson;
+    private SharedPreferences mPreferences;
 
     private List<Channel> mChannelsList;
 
+    @MainThread
     public ChannelManager(Gson gson, SharedPreferences preferences) {
         mGson = gson;
         mPreferences = preferences;
+    }
+
+    @MainThread
+    public List<Channel> getChannelsList() {
+        if (mChannelsList == null) {
+            mChannelsList = readChannelsList();
+        }
+        return mChannelsList;
+    }
+
+    @MainThread
+    public void addChannel(Channel channel) {
+        mChannelsList.add(channel);
+        saveChannelsList(mChannelsList);
+    }
+
+    @MainThread
+    public void removeChannel(int position) {
+        mChannelsList.remove(position);
+        saveChannelsList(mChannelsList);
+    }
+
+    @MainThread
+    public void editChannel(int position, Channel channel) {
+        mChannelsList.remove(position);
+        mChannelsList.add(position, channel);
+        saveChannelsList(mChannelsList);
     }
 
     private String getJsonStringFromList(List<Channel> list) {
@@ -41,34 +68,11 @@ public class ChannelManager {
         }.getType());
     }
 
-    public void saveChannelsList(List<Channel> list) {
+    private void saveChannelsList(List<Channel> list) {
         mPreferences.edit().putString(PREF_CHANNELS_LIST, getJsonStringFromList(list)).commit();
     }
 
     private List<Channel> readChannelsList() {
         return getChannelsListFromString(mPreferences.getString(PREF_CHANNELS_LIST, "[]"));
-    }
-
-    public List<Channel> getChannelsList() {
-        if (mChannelsList == null) {
-            mChannelsList = readChannelsList();
-        }
-        return mChannelsList;
-    }
-
-    public void addChannel(Channel channel) {
-        mChannelsList.add(channel);
-        saveChannelsList(mChannelsList);
-    }
-
-    public void removeChannel(int position) {
-        mChannelsList.remove(position);
-        saveChannelsList(mChannelsList);
-    }
-
-    public void editChannel(int position, Channel channel) {
-        mChannelsList.remove(position);
-        mChannelsList.add(position, channel);
-        saveChannelsList(mChannelsList);
     }
 }
